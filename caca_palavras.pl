d6882b3208db75128bd0432b7bbd29232e3bc273
SuelanyBr <maria.cruz@ccc.ufcg.edu.br>
2017-08-15 17:05:48 -0300

    Achar Palavras

--- caca_palavras.pl
+++ caca_palavras.pl
@@ -1,43 +1,54 @@
-cls :- write('\e[H\e[2J').
+leIndices(L,C) :- write("Digite a Linha: "),
+				  read_line_to_codes(user_input, A3),
+				  string_to_atom(A3,A12),
+				  atom_number(A12,L),
+				 
+				  write("Digite a Coluna: "),
+				  read_line_to_codes(user_input, B3),
+				  string_to_atom(B3,B12),
+				  atom_number(B12,C).
+				  
+				  
+formaPalavra([H|T],P1) :- 
+	
+	leIndices(LI,COL),
+	procuraletra([H|T],LI,COL,LETRA),
+	
+	write(LETRA),nl,
+	
+	string_concat(P1,LETRA,X),
+	
+	write("Continuar? S/N "),
+	read_line_to_codes(user_input, A3),
+	string_to_atom(A3,A12),
+	atom_string(A12,A),
+	
+	write(X),nl,
+	
+	A = "S" -> formaPalavra([H|T],X); write("FIM"),nl. 
 
-leInteiro(N) :- read_line_to_codes(user_input, A3),
-				string_to_atom(A3,A12),
-				atom_number(A12,N).
-				
-leString(S) :- read_line_to_codes(user_input, P2),
-			   string_to_atom(P2,P1),
-			   atom_string(P1,S).
 
-imprimeMatriz([]) :- nl.
-imprimeMatriz(H|T) :- S is separaLetras(H),
-					  write("                 "), write(S), nl,
-					  imprimeMatriz(T).
+procuraletra([H|T],L,C,LETRA) :-
+		linha([H|T],L,X),
+		coluna(X,C,LETRA).
 
-separaLetras([P], [R]) :- string_concat(P, " ", R).
-separaLetras([P|Ps], [R|Rs]) :- string_concat(P, " ", R), separaLetras(Ps, Rs).
-								 
+linha(M,L,X) :-
+	nth0(L,M,X).
+	
+coluna(X,C,LETRA):-
+	atom_chars(X,L1),
+	nth0(C,L1,LETRA).
 
+
+	
+	
 :- initialization main.
 
 main :-
+	M = ["abcde","acdsa"],
 	
-	write(""), nl,
-	write("*********************JOGO DE CAÇA PALAVRAS*********************"), nl,
-	write(""), nl,
-	write("Intruções: "), nl,
-	write("   1 - Você irá digitar as 4 palavras que serão escondidas."), nl,
-	write("   2 - Você deverá digitar as palavras que serão escondidas uma por vez."), nl,
-	write("        - As palavras não devem possuir inteiros."), nl,
-	write("        - As palavras devem possuir entre 2 e 10 letras."), nl,
-	write("        - Não deve possuir palavras repetidas."), nl,
-	write("   3 - Para procurar as palavras você irá precisar informar as linhas e as colunas das palavras que será formada."), nl,
-	write("   4 - O jogo será finalizado quando todas as palavras forem encontradas."), nl,
-	write(""), nl,
-	write("Digite as palavras seguidas de um enter: "), nl,
+	P = "",
+	formaPalavra(M,P),
+	write(P),nl,
+	halt(0).
 	
-	/*?-random(0, 9999, X), .  gera numero aleatorio
-	
-	le primeira palavra*/
-	string_chars("abracadabra", Lista),
-	separaLetras(Lista, Separado),
-	write(Separado),nl,
