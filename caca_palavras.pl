fd71e02ee237b68ae0d0b34699979ccd2a394715
Suelany Brito <suelanybr@gmail.com>
2017-08-17 00:23:27 -0300

    Procura palavra em uma matriz

--- caca_palavras.pl
+++ caca_palavras.pl
@@ -1,3 +1,23 @@
+/**A partir de indices forma uma palavra e verifica se essa palavra esta em uma lista de palavras escodidas**/
+
+formaPalavra([H|T],P1,S) :- 
+	(P1\=="") -> x1([H|T],P1,S); x1([H|T],"",S).
+	
+x1([H|T],P1,S) :-
+	leIndices(LI,COL),
+	procuraletra([H|T],LI,COL,LETRA),
+	string_concat(P1,LETRA,X),
+	continuar([H|T],X,S).
+
+continuar(M,P,S):-
+	write("Continuar? S/N "),
+	read_line_to_codes(user_input, A3),
+	string_to_atom(A3,A12),
+	atom_string(A12,A),
+	
+	A = "S" -> formaPalavra(M,P,S); S=P. 
+
+	
 leIndices(L,C) :- write("Digite a Linha: "),
 				  read_line_to_codes(user_input, A3),
 				  string_to_atom(A3,A12),
@@ -8,26 +28,6 @@ leIndices(L,C) :- write("Digite a Linha: "),
 				  string_to_atom(B3,B12),
 				  atom_number(B12,C).
 				  
-				  
-formaPalavra([H|T],P1) :- 
-	
-	leIndices(LI,COL),
-	procuraletra([H|T],LI,COL,LETRA),
-	
-	write(LETRA),nl,
-	
-	string_concat(P1,LETRA,X),
-	
-	write("Continuar? S/N "),
-	read_line_to_codes(user_input, A3),
-	string_to_atom(A3,A12),
-	atom_string(A12,A),
-	
-	write(X),nl,
-	
-	A = "S" -> formaPalavra([H|T],X); write("FIM"),nl. 
-
-
 procuraletra([H|T],L,C,LETRA) :-
 		linha([H|T],L,X),
 		coluna(X,C,LETRA).
@@ -39,16 +39,21 @@ coluna(X,C,LETRA):-
 	atom_chars(X,L1),
 	nth0(C,L1,LETRA).
 
+encontrarPalavra(M,L,R) :-
+	formaPalavra(M,"",S),
+	find(L,S,R).
 
-	
-	
+find([],_,R) :- R = "Palavra nao foi encontrada".
+find([H|T],E,R) :-
+    (H\==E) -> find(T,E,R); R = "Palavra Encontrada".
+
 :- initialization main.
 
 main :-
-	M = ["abcde","acdsa"],
+	M = ["casads","sacdsa"],
+	L = ["patio","casa"],
 	
-	P = "",
-	formaPalavra(M,P),
-	write(P),nl,
-	halt(0).
+	
+	encontrarPalavra(M,L,R),
+	write(R),nl.
 	
