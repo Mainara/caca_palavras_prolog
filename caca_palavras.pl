0fd13944c2e7bfd23d2b3a6ac9ba1105949dc102
anarcoqr <anarcoqr@gmail.com>
2017-08-17 18:17:56 -0300

    adicionando vertical e diagonalmente

--- caca_palavras.pl
+++ caca_palavras.pl
@@ -14,37 +14,46 @@ leString(S) :- read_line_to_codes(user_input, P2),
 			   string_to_atom(P2,P1),
 			   atom_string(P1,S).
 
-cacaPalavras(["","","","","","","","","",""]).
-
-replace(_, _, [], []).
-replace(O, R, [O|T], [R|T2]) :- replace(O, R, T, T2).
-replace(O, R, [H|T], [H|T2]) :- H \= O, replace(O, R, T, T2).
+cacaPalavras(["1111111111","1111111111","1111111111","1111111111","1111111111","1111111111","1111111111","1111111111","1111111111","1111111111"]).
 
 letraAleatoria(L) :- random(0,25,X),
 					 nth0(X,['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'], L).  
 
+adicionaElemento(ContColuna,[L|Ls],Letra,ContColuna,[Letra|Ls]).
+adicionaElemento(ContColuna,[L|Ls],Letra,Coluna,[L|X]):-
+	N is ContColuna +1,
+	adicionaElemento(N,Ls,Letra,Coluna,X).
+	
+vertical(_,[M|Mt],[],_,_,[M|Mt]).
+vertical(ContLinha,[M|Mt],[P|Ps],ContLinha,Coluna,[Palavra|X]):-
+	string_chars(M,Lista),
+	adicionaElemento(0,Lista,P,Coluna,ListaRetorno),
+	string_chars(Palavra,ListaRetorno),
+	vertical(ContLinha,Mt,Ps,ContLinha,Coluna,X).
+	
+vertical(ContLinha,[M|Mt],[P|Ps],Linha,Coluna,[M|X]):- 
+	N is ContLinha +1,
+	vertical(N,Mt,[P|Ps],Linha,Coluna,X).
 
-preencheString(Palavra, 10, Palavra).
-preencheString(Palavra, Tamanho, Modificada) :- letraAleatoria(L), string_concat(Palavra, L, X), Z is Tamanho +1, preencheString(X, Z, Modificada).
-
-preencheMatriz([], R):-!.
-preencheMatriz([H|T], R) :-
-	string_chars(H, H1),
-	length(H1, Y),
-	preencheString(H, Y, R),
-	preencheMatriz(T,[R]).
-						 
-vertical(_, _, "", _, MatrizResultante).
-vertical(I, J, Palavra, Matriz, MatrizResultante) :- write("OK").
-
-
+diagonal(_,[M|Mt],[],_,_,[M|Mt]).
+diagonal(ContLinha,[M|Mt],[P|Ps],ContLinha,Coluna,[Palavra|X]):-
+	string_chars(M,Lista),
+	adicionaElemento(0,Lista,P,Coluna,ListaRetorno),
+	string_chars(Palavra,ListaRetorno),
+	N is Coluna +1,
+	diagonal(ContLinha,Mt,Ps,ContLinha,N,X).
+	
+diagonal(ContLinha,[M|Mt],[P|Ps],Linha,Coluna,[M|X]):- 
+	N is ContLinha +1,
+	diagonal(N,Mt,[P|Ps],Linha,Coluna,X).
+	
 :- initialization main.
 
 main :-
 	
-	write(""), nl,
+	nl,
 	write("*********************JOGO DE CAÇA PALAVRAS*********************"), nl,
-	write(""), nl,
+	nl,
 	write("Intruções: "), nl,
 	write("   1 - Você irá digitar as 4 palavras que serão escondidas."), nl,
 	write("   2 - Você deverá digitar as palavras que serão escondidas uma por vez."), nl,
@@ -53,7 +62,7 @@ main :-
 	write("        - Não deve possuir palavras repetidas."), nl,
 	write("   3 - Para procurar as palavras você irá precisar informar as linhas e as colunas das palavras que será formada."), nl,
 	write("   4 - O jogo será finalizado quando todas as palavras forem encontradas."), nl,
-	write(""), nl,
+	nl,
 	write("Digite as palavras seguidas de um enter: "), nl,
 	
 	/*?-random(0, 9999, X), .  gera numero aleatorio
@@ -61,5 +70,6 @@ main :-
 	*/
 	
 	cacaPalavras(X),
-	preencheMatriz(X, L),
-	write(L).
+	write(X),nl,
+	diagonal(0,X,['F','O','G','O'],0,3,R),
+	write(R).
